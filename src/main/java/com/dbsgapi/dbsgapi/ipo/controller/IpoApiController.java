e74087bfc5c57edd7895e95d0b0c9b6f6414d8ea
Basak <chgy2131@naver.com>
2022-01-28 14:51:14 +0900

    - Cors Filter 처리

--- src/main/java/com/dbsgapi/dbsgapi/ipo/controller/IpoApiController.java
+++ src/main/java/com/dbsgapi/dbsgapi/ipo/controller/IpoApiController.java
@@ -6,10 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +17,7 @@ public class IpoApiController {
     @Autowired
     private IpoService ipoService;
 
+    //@CrossOrigin(origins = "http://localhost:3000")
     @RequestMapping(value="", method = RequestMethod.GET)
     public ResponseEntity<List<IpoDto>> getIpoList() throws Exception {
         List<IpoDto> listIpo = ipoService.selectIpos();
