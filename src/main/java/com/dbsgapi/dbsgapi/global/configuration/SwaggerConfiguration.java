bb8d3582024cd2d4aad15f9f5a98427936964eb4
Basak <chgy2131@naver.com>
2022-09-14 01:16:07 +0900

    [feature] #22 - Swagger UI(website) 에서 jwt를 사용하여 request 요청 가능

--- src/main/java/com/dbsgapi/dbsgapi/global/configuration/SwaggerConfiguration.java
+++ src/main/java/com/dbsgapi/dbsgapi/global/configuration/SwaggerConfiguration.java
@@ -1,13 +1,17 @@
 package com.dbsgapi.dbsgapi.global.configuration;
 
 import com.dbsgapi.dbsgapi.global.configuration.properties.SwaggerProperty;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,10 +20,26 @@ public class SwaggerConfiguration {
 
     @Bean
     public OpenAPI springShopOpenAPI() {
-        return new OpenAPI().info(new Info()
+        // Security 스키마 설정
+        SecurityScheme bearerAuth = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name(HttpHeaders.AUTHORIZATION);
+
+        // Security 요청 설정
+        SecurityRequirement addSecurityItem = new SecurityRequirement();
+        addSecurityItem.addList("JWT");
+
+        // Swagger 생성
+        return new OpenAPI()
+                .info(new Info()
                         .title(swaggerProperty.getTitle())
                         .description(swaggerProperty.getDescription())
                         .version(swaggerProperty.getVersion())
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .components(new Components().addSecuritySchemes("JWT", bearerAuth))
+                .addSecurityItem(addSecurityItem);
     }
 }
