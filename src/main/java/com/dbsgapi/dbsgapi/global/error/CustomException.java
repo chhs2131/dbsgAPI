b0e7074edb375876a3e22d75d596f29b514e6d00
Basak <chgy2131@naver.com>
2022-09-17 11:46:21 +0900

    [feature] #24 - Custom Exception 동작 관련 클래스 추가

---
+++ src/main/java/com/dbsgapi/dbsgapi/global/error/CustomException.java
@@ -0,0 +1,10 @@
+package com.dbsgapi.dbsgapi.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
