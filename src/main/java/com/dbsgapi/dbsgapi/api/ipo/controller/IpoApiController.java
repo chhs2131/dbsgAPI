abae9f3f5aa5c76bc1681b3744687778924939c0
Basak <chgy2131@naver.com>
2022-09-30 23:25:30 +0900

    [docs] #9 - swagger 반환형식 표현 강제를 위해 @ApiResponse 적용

--- src/main/java/com/dbsgapi/dbsgapi/api/ipo/controller/IpoApiController.java
+++ src/main/java/com/dbsgapi/dbsgapi/api/ipo/controller/IpoApiController.java
@@ -5,6 +5,10 @@ import com.dbsgapi.dbsgapi.api.ipo.service.IpoService;
 import com.dbsgapi.dbsgapi.global.error.CustomException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,7 +31,8 @@ public class IpoApiController {
     private final IpoService ipoService;
 
     @GetMapping(value="")
-    @Operation(summary="IPO 목록을 반환", description="IPO 목록을 최근 등록된 순으로 반환합니다. (추후 페이징 방식으로 변경 예정)")
+    @Operation(summary="IPO 목록을 반환", description="IPO 목록을 최근 등록된 순으로 반환합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = IpoSummaryDto.class))))
     public ResponseEntity<List<IpoSummaryDto>> getIpoList(
             @Parameter(description="페이지 번호") @RequestParam(required=false, defaultValue="1") int page,
             @Parameter(description="페이지당 반환갯수") @RequestParam(required=false, defaultValue="100") int num,
@@ -44,6 +49,7 @@ public class IpoApiController {
     // TODO 단일 조회 path mapping uri 변경 필요 detail <->
     @GetMapping(value="/{ipoIndex}")
     @Operation(summary="단일 IPO 종목을 상세조회", description="ipoIndex에 해당하는 종목에 상세 정보를 반환합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = IpoDetailDto.class)))
     public ResponseEntity<IpoDetailDto> getIpo(@PathVariable("ipoIndex") long ipoIndex) throws Exception {
         IpoDetailDto ipoData = new IpoDetailDto();
         ipoData.setIpo(ipoService.selectIpo(ipoIndex));
@@ -57,6 +63,7 @@ public class IpoApiController {
     
     @GetMapping(value="/detail/{ipoIndex}")
     @Operation(summary="IPO 기본정보 확인", description="해당 종목에 기본정보를 조회합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = IpoDto.class)))
     public ResponseEntity<IpoDto> getIpoDetail(@PathVariable("ipoIndex") long ipoIndex) throws Exception {
         IpoDto ipoData = ipoService.selectIpo(ipoIndex);
 
@@ -67,6 +74,7 @@ public class IpoApiController {
 
     @GetMapping(value="/schedule")
     @Operation(summary="지정 기간내에 일정을 확인", description="지정한 기간내에 일정을 모두 확인합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = IpoSummaryDto.class))))
     public ResponseEntity<List<IpoSummaryDto>> getScheduleList(
             @Parameter(description="조회 시작일자") String startDate,
             @Parameter(description="조회 종료일자") String endDate) throws Exception {
@@ -80,6 +88,7 @@ public class IpoApiController {
 
     @GetMapping(value="/comment")
     @Operation(summary="IPO Comment 조회", description="코멘트(히스토리)를 조회합니다. 이 때, 최근 코멘트가 앞쪽 페이지에 위치합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = IpoCommentDto.class))))
     public ResponseEntity<List<IpoCommentDto>> getIpoCommentList(
             @Parameter(description="특정 ipoIndex만 조회") @RequestParam(required=false, defaultValue="0") int ipoIndex,
             @Parameter(description="조회 시작일자") @RequestParam(required = false, defaultValue = "#{T(java.time.LocalDate).now().minusDays(7)}")
@@ -103,6 +112,7 @@ public class IpoApiController {
 
     @GetMapping(value="/comment/{commentIndex}")
     @Operation(summary="특정 Comment 확인", description="단일 comment를 조회합니다. commentIndex를 통해 조회합니다. (ipoIndex 즉, 종목번호 아님)")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = IpoCommentDto.class)))
     public ResponseEntity<IpoCommentDto> getIpoComment(@PathVariable("commentIndex") long commentIndex) throws Exception {
         IpoCommentDto ipoData = ipoService.selectIpoCommentIndex(commentIndex);
 
@@ -113,6 +123,7 @@ public class IpoApiController {
 
     @GetMapping(value="/underwriter/{ipoIndex}")
     @Operation(summary="IPO 주간사 정보 확인", description="해당 종목에 주간사 정보를 조회합니다.")
+    @ApiResponse(responseCode = "200", content = @Content(array = @ArraySchema(schema = @Schema(implementation = IpoUnderwriterDto.class))))
     public ResponseEntity<List<IpoUnderwriterDto>> getIpoUnderwriter(@PathVariable("ipoIndex") long ipoIndex) throws Exception {
         List<IpoUnderwriterDto> ipoData = ipoService.selectIpoUnderwriter(ipoIndex);
 
