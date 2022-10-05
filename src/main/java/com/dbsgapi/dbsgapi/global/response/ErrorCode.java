4e41207c7b5c494ac932a9d62ec54c971979b968
Basak <chgy2131@naver.com>
2022-10-05 17:25:22 +0900

    [feature] #9 - errorCode의 httpStatus 구체화 (ipo 및 jwt)

--- src/main/java/com/dbsgapi/dbsgapi/global/response/ErrorCode.java
+++ src/main/java/com/dbsgapi/dbsgapi/global/response/ErrorCode.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.*;
 
 @Getter
 @AllArgsConstructor
@@ -14,20 +13,20 @@ public enum ErrorCode {
     TEST_EXCEPTION(NOT_FOUND, "T000", "yeah"),
 
     // ipo
-    IPO_LIST_NOT_FOUND_EXCEPTION(NOT_FOUND, "II000", "조건에 맞는 조회 결과가 없습니다."),
-    IPO_DETAIL_NOT_FOUND_EXCEPTION(NOT_FOUND, "II001", "조회 결과가 없습니다."),
-    IPO_SCHEDULE_NOT_FOUND_EXCEPTION(NOT_FOUND, "IS000", "조건에 맞는 조회 결과가 없습니다."),
-    IPO_COMMENT_LIST_NOT_FOUND_EXCEPTION(NOT_FOUND, "IC000", "조건에 맞는 조회 결과가 없습니다."),
-    IPO_COMMENT_NOT_FOUND_EXCEPTION(NOT_FOUND, "IC001", "조회 결과가 없습니다."),
+    IPO_LIST_NOT_FOUND_EXCEPTION(NO_CONTENT, "II000", "조건에 맞는 조회 결과가 없습니다."),
+    IPO_DETAIL_NOT_FOUND_EXCEPTION(NO_CONTENT, "II001", "조회 결과가 없습니다."),
+    IPO_SCHEDULE_NOT_FOUND_EXCEPTION(NO_CONTENT, "IS000", "조건에 맞는 조회 결과가 없습니다."),
+    IPO_COMMENT_LIST_NOT_FOUND_EXCEPTION(NO_CONTENT, "IC000", "조건에 맞는 조회 결과가 없습니다."),
+    IPO_COMMENT_NOT_FOUND_EXCEPTION(NO_CONTENT, "IC001", "조회 결과가 없습니다."),
     IPO_COMMENT_WRONG_PARAMETER_EXCEPTION(BAD_REQUEST, "IC100", "조회 대상이 올바르지 않습니다. (ipo index 오류)"),
-    IPO_UNDERWRITER_NOT_FOUND_EXCEPTION(NOT_FOUND, "IU000", "조회 결과가 없습니다."),
+    IPO_UNDERWRITER_NOT_FOUND_EXCEPTION(NO_CONTENT, "IU000", "조회 결과가 없습니다."),
 
     // jwt
-    MALFRMED_JWT_EXCEPTION(NOT_FOUND, "J000", "잘못된 JWT 서명입니다."),
-    EXPIRED_JWT_EXCEPTION(NOT_FOUND, "J001", "만료된 JWT 토큰입니다."),
-    UNSUPPORTED_JWT_EXCEPTION(NOT_FOUND, "J002", "지원되지 않는 JWT 토큰입니다."),
-    ILLEGAL_JWT_EXCEPTION(NOT_FOUND, "J003", "JWT 토큰이 잘못되었습니다."),
-    NULL_POINTER_JWT_EXCEPTION(NOT_FOUND, "J004", "토큰 값이 비어있습니다.")
+    MALFRMED_JWT_EXCEPTION(UNAUTHORIZED, "J000", "잘못된 JWT 서명입니다."),
+    EXPIRED_JWT_EXCEPTION(FORBIDDEN, "J001", "만료된 JWT 토큰입니다."),
+    UNSUPPORTED_JWT_EXCEPTION(BAD_REQUEST, "J002", "지원되지 않는 JWT 토큰입니다."),
+    ILLEGAL_JWT_EXCEPTION(BAD_REQUEST, "J003", "JWT 토큰이 잘못되었습니다."),
+    NULL_POINTER_JWT_EXCEPTION(BAD_REQUEST, "J004", "토큰 값이 비어있습니다.")
     ;
 
     private final HttpStatus status;
