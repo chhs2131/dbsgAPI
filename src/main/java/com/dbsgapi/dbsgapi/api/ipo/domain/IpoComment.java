62f20ba12b7c54da1d27660e71ddc080f1433c4c
Basak <chgy2131@naver.com>
2022-10-30 15:34:45 +0900

    [feature/#59] IpoComment Builder를 이용해 makeNew 메소드 (static) 생성

--- src/main/java/com/dbsgapi/dbsgapi/api/ipo/domain/IpoComment.java
+++ src/main/java/com/dbsgapi/dbsgapi/api/ipo/domain/IpoComment.java
@@ -1,9 +1,18 @@
 package com.dbsgapi.dbsgapi.api.ipo.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class IpoComment {
     private KindOfComment kind;
     private String detail;
+
+    public static IpoComment makeNew(KindOfComment kind, String detail) {
+        return IpoComment.builder()
+                .kind(kind)
+                .detail(detail)
+                .build();
+    }
 }
