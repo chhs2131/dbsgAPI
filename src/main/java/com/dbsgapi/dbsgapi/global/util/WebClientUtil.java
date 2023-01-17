b97704fcb8896633290add441689e55315056cf8
Basak <chgy2131@naver.com>
2023-01-17 23:29:28 +0900

    refactor(WebClientUtil): 작성되지 않은 클래스에 대해 주석처리

---
+++ src/main/java/com/dbsgapi/dbsgapi/global/util/WebClientUtil.java
@@ -0,0 +1,21 @@
+package com.dbsgapi.dbsgapi.global.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RequiredArgsConstructor
+public class WebClientUtil {
+
+    public WebClient kakaoExample() {
+        return null;
+//        return WebClient.builder()
+//                .baseUrl("")
+//                .
+//                .build();
+    }
+
+    /*
+    https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/reactive/function/client/WebClient.html
+    https://gngsn.tistory.com/154
+     */
+}
