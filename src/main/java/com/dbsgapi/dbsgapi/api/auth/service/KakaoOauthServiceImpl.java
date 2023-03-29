833ab1eb571394b7e2c42a8cbffc2611a9d3c1c5
Basak <chgy2131@naver.com>
2023-03-29 16:14:42 +0900

    refactor(KakaoOauthService): 직접 호출하도록 수정

--- src/main/java/com/dbsgapi/dbsgapi/api/auth/service/KakaoOauthServiceImpl.java
+++ src/main/java/com/dbsgapi/dbsgapi/api/auth/service/KakaoOauthServiceImpl.java
@@ -1,35 +1,21 @@
 package com.dbsgapi.dbsgapi.api.auth.service;
 
-import com.dbsgapi.dbsgapi.api.auth.dto.KakaoErrorDto;
 import com.dbsgapi.dbsgapi.api.auth.dto.KakaoProfileDto;
-import com.dbsgapi.dbsgapi.api.auth.dto.KakaoTokenInfoDto;
-import com.dbsgapi.dbsgapi.api.auth.exception.KakaoApiErrorCode;
-import com.dbsgapi.dbsgapi.api.auth.exception.KakaoApiException;
-import com.dbsgapi.dbsgapi.api.member.dto.MemberDto;
 import com.dbsgapi.dbsgapi.api.auth.dto.MemberOauthAccountDto;
 import com.dbsgapi.dbsgapi.api.auth.mapper.AuthMapper;
+import com.dbsgapi.dbsgapi.api.member.dto.MemberDto;
 import com.dbsgapi.dbsgapi.api.member.mapper.MemberMapper;
 import com.dbsgapi.dbsgapi.global.authentication.AuthResponse;
 import com.dbsgapi.dbsgapi.global.authentication.MemberPermission;
 import com.dbsgapi.dbsgapi.global.authentication.OauthType;
-import com.dbsgapi.dbsgapi.global.configuration.properties.SocialProperty;
-import com.dbsgapi.dbsgapi.global.configuration.properties.type.ApiProvider;
-import com.dbsgapi.dbsgapi.global.configuration.properties.type.ApiRequest;
-import com.dbsgapi.dbsgapi.global.infra.api.kakao.KakaoApiProvider;
 import com.dbsgapi.dbsgapi.global.infra.api.kakao.KakaoApiService;
 import com.dbsgapi.dbsgapi.global.util.JwtUtil;
-import com.dbsgapi.dbsgapi.global.util.WebClientUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-import java.util.function.Consumer;
 
 @Slf4j
 @Service
@@ -42,15 +28,14 @@ public class KakaoOauthServiceImpl implements KakaoOauthService {
 
     @Override
     public AuthResponse login(String kakaoAccessToken) {
-        KakaoProfileDto kakaoProfile = getProfile(kakaoAccessToken);
-        if (kakaoProfile == null) {
-            throw new IllegalArgumentException("유효하지않은 KakaoAccessToken 입니다.");
-        }
+        KakaoProfileDto kakaoProfile = kakaoApiService.getProfile(kakaoAccessToken).block();
 
-        MemberDto member = authMapper.getMemberByOauthAccount(MemberOauthAccountDto.builder()
+        MemberDto member = authMapper.getMemberByOauthAccount(
+                MemberOauthAccountDto.builder()
                 .oauthType(OauthType.KAKAO)
                 .oauthId(String.valueOf(kakaoProfile.getId()))
-                .build());
+                .build()
+        );
         if (member == null) {
             // 신규 회원 생성
             member = MemberDto.builder()
@@ -74,13 +59,5 @@ public class KakaoOauthServiceImpl implements KakaoOauthService {
         }
 
         return jwtUtil.createAuthResponse(member.getUuid(), member.getMemberPermissionType());
-    }
-
-    private KakaoTokenInfoDto getTokenInformation(String kakaoAccessToken) {
-        return kakaoApiService.getTokenInformation(kakaoAccessToken).block();
-    }
-
-    private KakaoProfileDto getProfile(String kakaoAccessToken) {
-        return kakaoApiService.getProfile(kakaoAccessToken).block();
     }
 }
